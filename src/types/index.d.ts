82a79aa4201f6a7425f324cc929f2f124b246072
Nikita <nikita.shkuratt@gmail.com>
2024-04-18 16:59:08 +0300

    upd types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,14 @@ declare module "*.module.scss" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "*.svg" {
+  // const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  const content: string;
+  export default content;
+}
+declare module "*.png" {
+  // const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  const content: string;
+  export default content;
+}
---
+++ src/types/propTypes.ts
@@ -0,0 +1,10 @@
+export type TFeatureProp = {
+  title: string;
+  text: string;
+};
+
+export type TArticleProp = {
+  imgUrl: string;
+  date: string;
+  text: string;
+};
