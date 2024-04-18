edb965e8e0bd6d7614fb9534f9c4ee6220eba450
Nikita <nikita.shkuratt@gmail.com>
2024-04-18 16:58:00 +0300

    add article(blog component)

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,10 +1,21 @@
-import styles from "./article.module.scss";
+import { TArticleProp } from "../../types/propTypes";
 
-const Article = () => {
+import "./article.scss";
+
+const Article = ({ imgUrl, date, text }: TArticleProp) => {
   return (
-    <>
-      <div className={styles.red}>Article</div>
-    </>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog img" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   );
 };
 
